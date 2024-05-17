01d24a3e7e7f7dd171caf9b0b892b03b9f172e3a
Rio Agustian <rioagustian2001@gmail.com>
2024-05-17 20:52:50 +0700

    first commit

--- webpack.common.js
+++ webpack.common.js
@@ -55,7 +55,7 @@ module.exports = {
         }),
       ],
     }),
-    new BundleAnalyzerPlugin(),
+    new BundleAnalyzerPlugin({ analyzerMode: "static", openAnalyzer: false }),
     new WorkboxWebpackPlugin.GenerateSW({
       swDest: "./sw.bundle.js",
       runtimeCaching: [
